40d8a1aeb1ed330facb676cffb12f7d6ab1c44d7
Nitrozer <girito.erena@gmail.com>
2026-03-29 18:40:33 +0200

    fix: use npm package with postinstall patch instead of broken local fork

--- packages/expo-pencilkit-ui/build/index.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-/**
- * PencilKit View Component
- */
-export declare const PencilKitView: React.ForwardRefExoticComponent<React.RefAttributes<unknown>>;
-//# sourceMappingURL=index.d.ts.map
